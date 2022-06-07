b027fb0074d8b75118d05cf4cbf1d47bb493a294
basharbakkar <basharinitial.bakkar@gmail.com>
2022-06-07 22:12:31 +0300

    edit pagination

--- frontend/src/Components/allMenue/AllMenue.js
+++ frontend/src/Components/allMenue/AllMenue.js
@@ -1,49 +1,88 @@
-import axios from "axios"
-import {useState,useEffect} from "react"
-import { setMeals } from "../../redux/reducers/meals/index"
-import {useSelector,useDispatch} from "react-redux"
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { setMeals } from "../../redux/reducers/meals/index";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
-const AllMenue=(req,res)=>{
-   const [page,setPage]=useState()
-    /*     const [meal,setMeal]=useState(``)
- */    const [message,setMessage]=useState(``)
-    const dispatch=useDispatch()
-     const{meals}=useSelector((state)=>{
-        return {
-            meals:state.meals.meals
+const AllMenue = (req, res) => {
+  const [allMeals, setAllMeals] = useState([]);
+  const [length,setLength]=useState([])
+  const [page, setPage] = useState(1);
+  /*     const [meal,setMeal]=useState(``)
+   */ const [message, setMessage] = useState(``);
+  const dispatch = useDispatch();
+  const { meals } = useSelector((state) => {
+    return {
+      meals: state.meals.meals,
+    };
+  });
 
-        }
-    })
-   
-    useEffect(() => {
-       
-        axios.get(`http://localhost:5000/meals/paginated?p=${page}`).then((result)=>{
-            dispatch(setMeals(result.data.products))
-        }).catch((err)=>{
-             setMessage(err.sqlMessage)
-        })
-    }, [page]);
 
-    
-    return (
-        <div key={"cc"}>
-            {meals.length&&meals.map((meal,index)=>{
-               return (
-                    <>
-                    <p key={meal.meal_name}>{meal.meal_name}</p>
-                    <p key={meal.meal_price}>{meal.meal_price}</p>
-                    <img src={meal.image}  alt=""  key={meal.id}/>
-                    <button>Add to Cart</button>
-                    {message}
 
-                    </>
-                )
-            })}
+  const paginated = () => {};
 
-           
+  const decrease = () => {
+    return setPage(page - 1);
+  };
 
-        </div>
-    )
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/meals/paginated?p=${page}`)
+      .then((result) => {
+        dispatch(setMeals(result.data.products));
+      })
+      .catch((err) => {
+        setMessage(err.sqlMessage);
+      });
+    axios.get(`http://localhost:5000/meals`).then((result) => {
+      setAllMeals(result.data.result);
+    });
+  }, [page]);
+
+  return (
+    <div key={"cc"}>
+        {  allMeals.length? console.log(allMeals.length):""
 }
+      {meals.length &&
+        meals.map((meal, index) => {
+          return (
+            <>
+              <p key={meal.meal_name}>{meal.meal_name}</p>
+              <p key={meal.meal_price}>{meal.meal_price}</p>
+              <img src={meal.image} alt="" key={meal.id} />
+              <button>Add to Cart</button>
+              {message}
+            </>
+          );
+        })}
 
-export default AllMenue
+<div class="center">
+  <div class="pagination">
+  <Link href="#">&laquo;</Link> 
+  {
+
+      allMeals.length=(allMeals.length/20)
+  } 
+  <Link href="#">&raquo;</Link>
+  </div>
+</div>
+
+      <button
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        Next
+      </button>
+      <button
+        onClick={() => {
+          setPage(page - 1);
+        }}
+      >
+        previous
+      </button>
+    </div>
+  );
+};
+
+export default AllMenue;
